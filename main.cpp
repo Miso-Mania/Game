be71e8e96adb7a108435f164f8ac2f195adf80dc
Claquettes <math@claq.fr>
2023-01-21 10:47:56 +0100

    square responds to keypresses

---
+++ main.cpp
@@ -0,0 +1,76 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("Square", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Initial position of the square
+    int x = 320, y = 240;
+
+    bool quit = false;
+    SDL_Event event;
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+            if (event.type == SDL_KEYDOWN) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_UP:
+                        y -= 10;
+                        break;
+                    case SDLK_DOWN:
+                        y += 10;
+                        break;
+                    case SDLK_LEFT:
+                        x -= 10;
+                        break;
+                    case SDLK_RIGHT:
+                        x += 10;
+                        break;
+                }
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_Rect rect;
+        rect.x = x;
+        rect.y = y;
+        rect.w = 50;
+        rect.h = 50;
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &rect);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
+
--- makefile
+++ makefile
@@ -1,19 +1,12 @@
-#OBJS specifies which files to compile as part of the project
-OBJS = 01_hello_SDL.cpp
-
-#CC specifies which compiler we're using
 CC = g++
+CFLAGS = $(shell sdl2-config --cflags)
+LIBS = $(shell sdl2-config --libs) -lSDL2_image
 
-#COMPILER_FLAGS specifies the additional compilation options we're using
-# -w suppresses all warnings
-COMPILER_FLAGS = -w
+all: main
 
-#LINKER_FLAGS specifies the libraries we're linking against
-LINKER_FLAGS = -lSDL2
+main: main.cpp
+	$(CC) $(CFLAGS) main.cpp -o main $(LIBS)
 
-#OBJ_NAME specifies the name of our exectuable
-OBJ_NAME = 01_hello_SDL
+clean:
+	rm -f main
 
-#This is the target that compiles our executable
-all : $(OBJS)
-	$(CC) $(OBJS) $(COMPILER_FLAGS) $(LINKER_FLAGS) -o $(OBJ_NAME)
