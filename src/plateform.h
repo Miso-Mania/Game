1b7bf0b7d32a5480722842b93cdb0ca160bd3500
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-02-10 23:38:14 +0100

    Plateform.h

---
+++ src/plateform.h
@@ -0,0 +1,15 @@
+#ifndef PLATFORM_H
+#define PLATFORM_H
+#include <SDL2/SDL.h>
+
+class Platform {
+public:
+    Platform();
+    Plateform(int x, int y, int w, int h);
+    ~Platform();
+    void draw(SDL_Renderer *renderer);
+    SDL_Rect getRect();
+    bool isColliding(Player player);
+private:
+    SDL_Rect rect;
+};
