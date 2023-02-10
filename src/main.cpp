33f627e2aa71c8eb23551f5dd68bf13ddd7ff3c7
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-02-10 23:59:15 +0100

    main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "game.h"
+using namespace std;
+
+int main(int argc, char *argv[])) {
+    Game *game = new Game();
+    game->init("Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+    while (game->running()) {
+        game->handleEvents();
+        game->update();
+        game->render();
+    }
+    game->clean();
+    return 0;
+}
