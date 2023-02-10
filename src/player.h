7bb12232b4742069201151052cc07bb09d6b3f17
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-02-10 23:28:10 +0100

    Plyer.h

---
+++ src/player.h
@@ -0,0 +1,18 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <SDL2/SDL.h>
+
+class Player {
+public:
+    Player();
+    ~Player();
+    void moveLeft();
+    void moveRight();
+    void jump();
+    void uptadePosition();
+    void draw(SDL_Renderer *renderer);
+    SDL_Rect getRect();
+private:
+    SDL_Rect rect;
+    int speed_x, speed_y;
+};
