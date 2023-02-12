40c4d15f87a9693a02cb1b5805115167bcf08072
Jules <jules.ginhac@etu.univ-lyon1.fr>
2023-02-12 22:08:21 +0100

    correction level.h

--- src/level.h
+++ src/level.h
@@ -11,11 +11,13 @@ public:
     void addObstacle(int x, int y, int width, int height);
     void update(double delta);
     void render(SDL_Renderer *renderer);
-    std::vector<Obstacle> getObstacles();
+    std::vector<Obstacle*> getObstacles();
+
+
     
 
 private:
-    std::vector<Obstacle> m_obstacles;
+    std::vector<Obstacle*> m_obstacles;
 };
 
-#endif // LEVEL_H
+#endif // LEVEL_HSS
