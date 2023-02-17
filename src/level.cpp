fca1dcb49ca7706f491d00a90d2a5c51bf9b9b32
Jules <jules.ginhac@etu.univ-lyon1.fr>
2023-02-17 18:12:23 +0100

    plus de warning dans le dourelement du make

--- src/level.cpp
+++ src/level.cpp
@@ -19,16 +19,6 @@ vector<Obstacle*> Level::getObstacles() {
     return m_obstacles;
 }
 
-void Level::removeObstacle(Obstacle* obstacle) {
-    for ( int i = 0; i < m_obstacles.size(); i++ ) {
-        if ( m_obstacles[i] == obstacle ) {
-            m_obstacles.erase(m_obstacles.begin() + i);
-            delete obstacle;
-            break;
-        }
-    }
-}
-
 void Level::addPic(int x, int y) {
     Pic* pic = new Pic(x, y);
     m_pics.push_back(pic);
@@ -38,15 +28,6 @@ vector<Pic*> Level::getPics() {
     return m_pics;
 }
 
-void Level::removePic(Pic* pic) {
-    for ( int i = 0; i < m_pics.size(); i++ ) {
-        if ( m_pics[i] == pic ) {
-            m_pics.erase(m_pics.begin() + i);
-            delete pic;
-            break;
-        }
-    }
-}
 
 
 
--- src/level.h
+++ src/level.h
@@ -12,10 +12,8 @@ public:
     ~Level();
     void addObstacle(int x, int y, int width, int height);
     vector<Obstacle*> getObstacles();
-    void removeObstacle(Obstacle* obstacle);
     void addPic(int x, int y);
     vector<Pic*> getPics();
-    void removePic(Pic* pic);
 
 private:
     vector<Obstacle*> m_obstacles;
