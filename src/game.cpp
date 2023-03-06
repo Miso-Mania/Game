8bba6b0b5531cd9447c4f1d66c639af835eacc80
Claquettes <math@claq.fr>
2023-03-06 09:46:17 +0100

    added tiles const

--- src/game.cpp
+++ src/game.cpp
@@ -6,6 +6,11 @@
 const int FPS = 60;
 const int window_X_size = 1900;
 const int window_Y_size = 1068;
+const int NUM_TILES_X = 64;
+const int NUM_TILES_Y = 36;
+const int TILE_SIZE_X = window_X_size / NUM_TILES_X;
+const int TILE_SIZE_Y = window_Y_size / NUM_TILES_Y;
+
 
 Game::Game() : m_window(NULL), m_renderer(NULL), m_currentLevel(0)
 {
