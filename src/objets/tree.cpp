3dcaaf54e146f6c8ecc89cf79d594815564b1a38
Claquettes <math@claq.fr>
2023-03-12 20:57:35 +0100

    correction of trees size + added in level1

--- src/objets/tree.cpp
+++ src/objets/tree.cpp
@@ -4,8 +4,8 @@ Tree::Tree(int x, int y, int TILE_SIZE) {
     m_coords = Coords(x, y, 3, 2);
     m_rect.x = x * TILE_SIZE;
     m_rect.y = y * TILE_SIZE;
-    m_rect.w = 3 * TILE_SIZE;
-    m_rect.h = 2 * TILE_SIZE;
+    m_rect.w = 5 * TILE_SIZE;
+    m_rect.h = 3 * TILE_SIZE;
 }
 
 Tree::~Tree() {
