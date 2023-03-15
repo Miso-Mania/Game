703bc885ef99d4c71126e79ff48ec48fe375ee90
Jules <jules.ginhac@etu.univ-lyon1.fr>
2023-03-15 14:06:53 +0100

    documentation classe music

--- src/music.h
+++ src/music.h
@@ -7,17 +7,39 @@
 
 class Music {
 public:
+/**
+    @brief Constructeur de la classe Music
+    @param std::string& filename : le nom du fichier audio
+*/
     Music(const std::string& filename);
+/**
+    @brief Destructeur de la classe Music
+    @param aucun
+    */
     ~Music();
-
+/**
+    @brief Fonction qui permet de savoir si la musique est en train de jouer
+    @param aucun
+*/
     bool isPlaying() const;
-
+/**
+    @brief Fonction qui permet de jouer la musique
+    @param aucun
+*/
     void play();
+/**
+    @brief Fonction qui permet de mettre en pause la musique    
+    @param aucun
+*/
     void pause();
+/**
+    @brief Fonction qui permet d'arrêter la musique
+    @param aucun
+*/
     void stop();
 
 private:
-    Mix_Music* m_music;
+    Mix_Music* m_music; // la musique 
 };
 
 #endif
