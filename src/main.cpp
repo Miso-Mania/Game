67ead7d92e0d42cb8a2afb74e75569b73d6e0ed0
Claquettes <math@claq.fr>
2023-03-18 23:44:11 +0100

    splash screen

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,14 @@ int main(int argc, char* argv[]) {
     SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_Surface* imageSurface = SDL_LoadBMP("assets/textures/back.png");
+    SDL_Surface* imageSurface = IMG_Load("assets/textures/menu-splashscreen.png");
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
+    if (texture == NULL) {
+        cout << "feur " << SDL_GetError() << endl;
+    }
+    if (imageSurface == NULL) {
+        cout << "feur mais 2" << SDL_GetError() << endl;
+    }
 
     SDL_Rect dstRect = {0, 0, 800, 600};
     SDL_RenderCopy(renderer, texture, NULL, &dstRect);
