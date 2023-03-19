d2a91fbf0bfc187a1a10fad7ecde6e4165739bc6
Claquettes <math@claq.fr>
2023-03-19 14:19:59 +0100

    added new splash art

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,29 @@
 #include "game.h"
 
 using namespace std;
-int main(int argc, char* argv[]) {
+void menu() {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+    SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 400, 400, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_Surface* imageSurface = IMG_Load("assets/textures/menusplash.png");
+    SDL_Surface* imageSurface = IMG_Load("assets/textures/splash.png");
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
-    if (texture == NULL) {
-        cout << "feur " << SDL_GetError() << endl;
-    }
-    if (imageSurface == NULL) {
-        cout << "feur mais 2" << SDL_GetError() << endl;
-    }
-
-    SDL_Rect dstRect = {0, 0, 800, 600};
+    SDL_Rect dstRect = {0, 0, 400, 400};
     SDL_RenderCopy(renderer, texture, NULL, &dstRect);
     SDL_RenderPresent(renderer);
 
-    SDL_Delay(3000);
+    SDL_Delay(1000); // on attends une seconde avant d'ouvrir le jeu
 
     SDL_DestroyTexture(texture);
     SDL_FreeSurface(imageSurface);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+}
 
+int main(int argc, char* argv[]) {
+    
+    menu();
     Game game;
     game.run();
     SDL_Quit();
