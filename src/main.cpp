31ef353fb6182c8b25838195e8ba0857d8947fdd
Claquettes <math@claq.fr>
2023-03-19 14:27:49 +0100

    added a sound when launching the game

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
 #include "game.h"
+#include <iostream>
+#include <SDL2/SDL_mixer.h>
 
 using namespace std;
 void menu() {
+    // Initialisation de la SDL
     SDL_Init(SDL_INIT_VIDEO);
 
     SDL_Window* window = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 400, 400, 0);
@@ -11,10 +13,32 @@ void menu() {
     SDL_Surface* imageSurface = IMG_Load("assets/textures/splash.png");
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, imageSurface);
     SDL_Rect dstRect = {0, 0, 400, 400};
+    //initialisation de la musique
+    if (Mix_Init(MIX_INIT_MP3) != MIX_INIT_MP3)
+    {
+        std::cerr << "Failed to initialize SDL2 Mixer: " << Mix_GetError() << std::endl;
+    }
+
+    if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 1024) != 0)
+    {
+        std::cerr << "Failed to open audio device: " << Mix_GetError() << std::endl;
+    }
+
+    Mix_Music *intro = Mix_LoadMUS("assets/music/intro.mp3");
+    if (!intro)
+    {
+        std::cerr << "Failed to load intro file: " << Mix_GetError() << std::endl;
+    }
+
+    if (Mix_PlayMusic(intro, -1) != 0)
+    {
+        std::cerr << "Failed to play intro: " << Mix_GetError() << std::endl;
+    }
+    Mix_PlayMusic(intro, 1);
     SDL_RenderCopy(renderer, texture, NULL, &dstRect);
     SDL_RenderPresent(renderer);
 
-    SDL_Delay(1000); // on attends une seconde avant d'ouvrir le jeu
+    SDL_Delay(1500); // on attends une seconde avant d'ouvrir le jeu
 
     SDL_DestroyTexture(texture);
     SDL_FreeSurface(imageSurface);
