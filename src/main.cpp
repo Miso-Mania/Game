d6d1b87bea0103c055f7da22dd4b95b95f9e8cf8
Claquettes <math@claq.fr>
2023-03-19 15:18:39 +0100

    ask for level

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <SDL2/SDL_mixer.h>
 
 using namespace std;
-void menu() {
+void menu(int i, int l) {
     // Initialisation de la SDL
     SDL_Init(SDL_INIT_VIDEO);
 
@@ -52,18 +52,40 @@ void menu() {
     SDL_Rect inputRect = {0, 0, 1280, 720};
     SDL_RenderCopy(inputRenderer, inputTexture, NULL, &inputRect);
     SDL_RenderPresent(inputRenderer);
-    
-    SDL_Delay(1500); // on attends une seconde avant d'ouvrir le jeu
-    SDL_DestroyTexture(inputTexture);
-    SDL_FreeSurface(inputSurface);
-    SDL_DestroyRenderer(inputRenderer);
-    SDL_DestroyWindow(inputWindow);
-    
+    //on demande le type d'input
+    cin >> i;
+    //si l'input est clavier; on ferme la fenêtre
+    if (i == 1 || i == 2) {
+        //on ferme la fenêtre
+        SDL_DestroyTexture(inputTexture);
+        SDL_FreeSurface(inputSurface);
+        SDL_DestroyRenderer(inputRenderer);
+        SDL_DestroyWindow(inputWindow);
+        //on ouvre le menu suivant
+        SDL_Window* levelWindow = SDL_CreateWindow("SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
+        SDL_Renderer* levelRenderer = SDL_CreateRenderer(levelWindow, -1, SDL_RENDERER_ACCELERATED);
+        SDL_Surface* levelSurface = IMG_Load("assets/textures/level.png");
+        SDL_Texture* levelTexture = SDL_CreateTextureFromSurface(levelRenderer, levelSurface);
+        SDL_Rect levelRect = {0, 0, 1280, 720};
+        SDL_RenderCopy(levelRenderer, levelTexture, NULL, &levelRect);
+        SDL_RenderPresent(levelRenderer);
+        //on demande le niveau
+        cin >> l;
+        //si le niveau est 1; on ferme la fenêtre
+        if (l == 1 || l == 2 || l == 3 || l == 4 || l == 5 || l == 6 || l == 7 || l == 8 || l == 9 || l == 10) {
+            SDL_DestroyTexture(levelTexture);
+            SDL_FreeSurface(levelSurface);
+            SDL_DestroyRenderer(levelRenderer);
+            SDL_DestroyWindow(levelWindow);
+        }
+    }
 }   
 
 int main(int argc, char* argv[]) {
+    int input = 444;
+    int level = 445;
     
-    menu();
+    menu(input, level);
     Game game;
     game.run();
     SDL_Quit();
